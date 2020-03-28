9666d23c9e8ca9f9d7fb05eb1863bac24b83b2a6
Robert Salzer <robert.salzer@garzotto.com>
2020-03-29 00:51:14 +0100

    add design

--- script.js
+++ script.js
@@ -26,10 +26,12 @@ var ageColours = {
   "missing": 'rgb(100,100,100)'
 };
 
-d3.json('https://api.github.com/repos/rsalzer/COVID_19_AGE/commits?path=allages.csv&page=1&per_page=1', function(error, data) {
-  var lastUpdateDiv = document.getElementById('latestUpdate');
-  lastUpdateDiv.innerHTML = "<i>Letztes Update der Daten: "+data[0].commit.committer.date.substring(0,10)+" ("+data[0].commit.message+")</i>";
-});
+Chart.defaults.global.defaultFontFamily = "IBM Plex Sans";
+
+// d3.json('https://api.github.com/repos/rsalzer/COVID_19_AGE/commits?path=allages.csv&page=1&per_page=1', function(error, data) {
+//   var lastUpdateDiv = document.getElementById('latestUpdate');
+//   lastUpdateDiv.innerHTML = "<i>Letztes Update der Daten: "+data[0].commit.committer.date.substring(0,10)+" ("+data[0].commit.message+")</i>";
+// });
 
 
 d3.csv('allages.csv', function(error, csvdata) {
@@ -45,14 +47,17 @@ d3.csv('allages.csv', function(error, csvdata) {
 d3.csv('allagesdetails.csv', function(error, csvdata) {
   detaildata = csvdata;
   var latest = detaildata[detaildata.length-1];
+  var keys = Object.keys(latest);
+  var latestArray = keys.map(function(key){
+    return latest[key];
+  });
+  var ftotal = latestArray.slice(1,10).reduce(function(acc, val) { return acc + parseInt(val); }, 0);
+  var mtotal = latestArray.slice(11,20).reduce(function(acc, val) { return acc + parseInt(val); }, 0);
   var div = document.getElementById("latest");
-  div.innerHTML = "<h3>Aktuelle absolute Zahlen der positiv Getesteten "+latest.date+"</h3>"
+  div.innerHTML = "<h3>Positiv getestet bis zum "+latest.date+"</h3>"
   var table = document.createElement("table");
   table.id = "firstTable";
-  table.innerHTML = "<tr><th>Altersgruppe</th><th>Frauen</th><th>Männer</th><th>Gesamt</th></tr>";
-  var sum = 0;
-  var sumf = 0;
-  var summ = 0;
+  table.innerHTML = "<tr><th>Altersgruppe</th><th>Frauen</th><th>%</th><th>Männer</th><th>%</th><th>Gesamt</th><th>%</th></tr>";
   var totalArray = [];
   for(var i=0; i<ageLabels.length; i++) {
     var tr = document.createElement("tr");
@@ -60,25 +65,30 @@ d3.csv('allagesdetails.csv', function(error, csvdata) {
     var f = parseInt(latest["f"+label]);
     var m = parseInt(latest["m"+label]);
     var tot = f+m;
-    totalArray.push(tot);
-    tr.innerHTML = "<th>"+label+"</th><td>"+f+"</td><td>"+m+"</td><td>"+tot+"</td>";
+    var percentagef = Math.round(f/ftotal*1000)/10;
+    var percentagem = Math.round(m/mtotal*1000)/10;
+    var percentagetot = Math.round(tot/(ftotal+mtotal)*1000)/10;
+    tr.innerHTML = "<th>"+label+"</th><td>"+f+"</td><td>"+percentagef+"%</td><td>"+m+"</td><td>"+percentagem+"%</td><td>"+tot+"</td><td>"+percentagetot+"%</td>";
     table.appendChild(tr);
-    sum += tot;
-    summ += m;
-    sumf += f;
   }
   var bag = parseInt(latest.totalbag);
-  var missing = bag-sum;
+  var all = ftotal+mtotal;
+  var missing = bag-all;
   var missingPerc = Math.round(missing/bag*100);
-  totalArray.push(missing);
   var tr = document.createElement("tr");
-  tr.innerHTML = "<th>Fehlend</th><td></td><td></td><td>"+missing+" ("+missingPerc+" %)</td>";
+  var percentagef = Math.round(ftotal/all*1000)/10;
+  var percentagem = Math.round(mtotal/all*1000)/10;
+  tr.innerHTML = "<th>TOTAL</th><td>"+ftotal+"</td><td>"+percentagef+"%</td><td>"+mtotal+"</td><td>"+percentagem+"%</td><td>"+all+"</td><td></td>";
   table.appendChild(tr);
   tr = document.createElement("tr");
-  tr.innerHTML = "<th>TOTAL</th><td>"+sumf+"</td><td>"+summ+"</td><td>"+bag+"</td>";
+  tr.innerHTML = "<th colspan=3>Ohne Altersangabe</th><td></td><td></td><td>"+missing+"</td><td>"+missingPerc+"%</td>";
+  table.appendChild(tr);
+  tr = document.createElement("tr");
+  tr.innerHTML = "<th colspan=3>Zahl aller Meldungen</th><td></td><td></td><td>"+bag+"</td><td></td>";
   table.appendChild(tr);
   div.appendChild(table);
 
+  /*
   var canvas = document.createElement("canvas");
   //canvas.className  = "myClass";
   canvas.id = 'piechart';
@@ -118,7 +128,7 @@ d3.csv('allagesdetails.csv', function(error, csvdata) {
     }
     };
     var chart = new Chart(canvas.id,config);
-
+    */
     loadDeaths();
 });
 
@@ -128,16 +138,27 @@ function loadDeaths() {
     deaths = csvdata;
     var latestDeaths = deaths[deaths.length-1];
     var latest = detaildata[detaildata.length-1];
+    var keys = Object.keys(latestDeaths);
+    var latestDeathsArray = keys.map(function(key){
+      return latestDeaths[key];
+    });
+    var ftotal = latestDeathsArray.slice(1,10).reduce(function(acc, val) { return acc + parseInt(val); }, 0);
+    var mtotal = latestDeathsArray.slice(10,19).reduce(function(acc, val) { return acc + parseInt(val); }, 0);
     var div = document.getElementById("latest");
     var h3 = document.createElement("h3");
-    h3.innerHTML = "Todesfälle bis zum "+latestDeaths.date+"</h3>";
+    h3.innerHTML = "Todesfälle bis zum "+latestDeaths.date;
     div.appendChild(h3);
     var table = document.createElement("table");
     table.id = "deathTable";
-    table.innerHTML = "<tr><th>Altersgruppe</th><th>Frauen</th><th>Sterblichkeit</th><th>Männer</th><th>Sterblichkeit</th><th>Gesamt</th><th>Sterblichkeit</th></tr>";
+    var mortalitytable = document.createElement("table");
+    mortalitytable.id = "mortalityTable";
+    table.innerHTML = "<tr><th>Altersgruppe</th><th>Frauen</th><th>%</th><th>Männer</th><th>%</th><th>Gesamt</th><th>%</th></tr>";
+    mortalitytable.innerHTML = "<tr><th>Altersgruppe</th><th>Frauen</th><th>Männer</th><th>Gesamt</th></tr>";
     var sum = 0;
     var sumf = 0;
     var summ = 0;
+    var sumcasesm = 0;
+    var sumcasesf = 0;
     var totalArray = [];
     for(var i=0; i<ageLabels.length; i++) {
       var tr = document.createElement("tr");
@@ -147,25 +168,44 @@ function loadDeaths() {
       var m = parseInt(latestDeaths["m"+label]);
       var mcases = parseInt(latest["m"+label]);
       var tot = f+m;
+      var percf = Math.round(f/ftotal*1000)/10;
+      var percm = Math.round(m/mtotal*1000)/10;
+      var perctot = Math.round(tot/(ftotal+mtotal)*1000)/10;
       var totcases = mcases + fcases;
       var fmortality = Math.round(f/fcases*100*100)/100;
       var mmortality = Math.round(m/mcases*100*100)/100;
       var totmortality = Math.round(tot/totcases*100*100)/100;
       totalArray.push(tot);
-      tr.innerHTML = "<th>"+label+"</th><td>"+f+"</td><td>"+fmortality+"%</td><td>"+m+"</td><td>"+mmortality+"%</td><td>"+tot+"</td><td>"+totmortality+"%</td>";
+      tr.innerHTML = "<th>"+label+"</th><td>"+f+"</td><td>"+percf+"%</td><td>"+m+"</td><td>"+percm+"%</td><td>"+tot+"</td><td>"+perctot+"%</td>";
       table.appendChild(tr);
       sum += tot;
       summ += m;
       sumf += f;
+      sumcasesm += mcases;
+      sumcasesf += fcases;
+      tr = document.createElement("tr");
+      tr.innerHTML = "<th>"+label+"</th><td>"+fmortality+"%</td><td>"+mmortality+"%</td><td>"+totmortality+"%</td>";
+      mortalitytable.appendChild(tr);
     }
 
     var tr = document.createElement("tr");
+    var fmortality = Math.round(sumf/sumcasesf*100*100)/100;
+    var mmortality = Math.round(summ/sumcasesm*100*100)/100;
+    var totmortality = Math.round(sum/parseInt(latest.totalbag)*100*100)/100;
+    var percf = Math.round(ftotal/(ftotal+mtotal)*1000)/10;
+    var percm = Math.round(mtotal/(ftotal+mtotal)*1000)/10;
+    tr.innerHTML = "<th>TOTAL</th><td>"+sumf+"</td><td>"+percf+"%</td><td>"+summ+"</td><td>"+percm+"%</td><td>"+sum+"</td><td></td>";
     table.appendChild(tr);
     tr = document.createElement("tr");
-    tr.innerHTML = "<th>TOTAL</th><td>"+sumf+"</td><td></td><td>"+summ+"</td><td></td><td>"+sum+"</td><td></td>";
-    table.appendChild(tr);
+    tr.innerHTML = "<th>TOTAL</th><td>"+fmortality+"%</td><td>"+mmortality+"%</td><td>"+totmortality+"%</td>";
+    mortalitytable.appendChild(tr);
     div.appendChild(table);
+    h3 = document.createElement("h3");
+    h3.innerHTML = "Mortalität bis zum "+latestDeaths.date+"</h3>";
+    div.appendChild(h3);
+    div.appendChild(mortalitytable);
 
+    /*
     var canvas = document.createElement("canvas");
     //canvas.className  = "myClass";
     canvas.id = 'piechartdeath';
@@ -204,7 +244,7 @@ function loadDeaths() {
       }
       };
       var chart = new Chart(canvas.id,config);
-
+      */
   });
 }
 
@@ -259,7 +299,7 @@ function pieChartSingleAgeSexLatest(sex) {
 }
 
 function chartSingleAgeSex(sex) {
-  var div = document.getElementById("maindiv");
+  var div = document.getElementById("detail");
   var canvas = document.createElement("canvas");
   //canvas.className  = "myClass";
   canvas.id = 'chart'+sex;
@@ -371,7 +411,7 @@ function chartSingleAgeSex(sex) {
 }
 
 function chartAgesBothSexes(age) {
-  var div = document.getElementById("maindiv");
+  var div = document.getElementById("detail");
   var canvas = document.createElement("canvas");
   //canvas.className  = "myClass";
   canvas.id = 'chart'+age;
