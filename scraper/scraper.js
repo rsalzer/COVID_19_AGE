d76e472c04757385674af0adfbb4c02b32c3c7d1
Robert Salzer <robert.salzer@garzotto.com>
2020-04-01 02:10:56 +0200

    add exports (ongoing)

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -5,6 +5,8 @@ const excelToJson = require('convert-excel-to-json');
 
 const bagExcelLocation = "https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/covid-19-datengrundlage-lagebericht.xlsx.download.xlsx/200325_Datengrundlage_Grafiken_COVID-19-Bericht.xlsx";
 
+const ageLabels = ["0-9","10-19","20-29","30-39","40-49","50-59","60-69","70-79","80+"];
+
 var myArgs = process.argv.slice(2);
 switch (myArgs[0]) {
 case 'download':
@@ -42,7 +44,7 @@ function parseExcel() {
   data.forEach((item, i) => {
     if(item.B!=null) {
       deaths.push({
-        age: item.A,
+        age: item.A.replace(/ /g,""),
         m: item.B,
         f: item.C
       })
@@ -58,7 +60,7 @@ function parseExcel() {
   data.forEach((item, i) => {
     if(item.B!=null) {
       hospitalised.push({
-        age: item.A,
+        age: item.A.replace(/ /g,""),
         m: item.B,
         f: item.C
       })
@@ -75,12 +77,12 @@ function parseExcel() {
   data.forEach((item, i) => {
     if(item.B!=null) {
       cases.push({
-        age: item.A,
+        age: item.A.replace(/ /g,""),
         m: item.B,
         f: item.E
       });
       incidences.push({
-        age: item.A,
+        age: item.A.replace(/ /g,""),
         m: item.D,
         f: item.G
       });
@@ -98,7 +100,51 @@ function parseExcel() {
 
   var dateString = totals[0].A
   var length = dateString.length;;
-  var subDate = dateString.substring(length-20,length-10);
-  console.log("Date: " + subDate);
+  var date = dateString.substring(length-20,length-10);
+  console.log("Date: " + date);
+
+  var allAgesDetailsCSV = "";
+  allAgesDetailsCSV += date;
+  ageLabels.forEach((item, i) => {
+    var singleCase = cases.filter(function(d) { if(d.age==item) return d});
+    if(singleCase.length>0) {
+      allAgesDetailsCSV += ","+singleCase[0].f;
+    }
+    else {
+      allAgesDetailsCSV += "0";
+    }
+  });
+  ageLabels.forEach((item, i) => {
+    var singleCase = cases.filter(function(d) { if(d.age==item) return d});
+    if(singleCase.length>0) {
+      allAgesDetailsCSV += ","+singleCase[0].m;
+    }
+    else {
+      allAgesDetailsCSV += ",0";
+    }
+  });
+  console.log("All ages details CSV: "+allAgesDetailsCSV);
+
+  var deathsCSV = "";
+  deathsCSV += date;
+  ageLabels.forEach((item, i) => {
+    var singleCase = deaths.filter(function(d) { if(d.age==item) return d});
+    if(singleCase.length>0) {
+      deathsCSV += ","+singleCase[0].f;
+    }
+    else {
+      deathsCSV += ",0";
+    }
+  });
+  ageLabels.forEach((item, i) => {
+    var singleCase = deaths.filter(function(d) { if(d.age==item) return d});
+    if(singleCase.length>0) {
+      deathsCSV += ","+singleCase[0].m;
+    }
+    else {
+      deathsCSV += ",0";
+    }
+  });
+  console.log("Deaths CSV: "+deathsCSV);
   //fs.unlink("temp.xlsx", function() { console.log("deleted") });
 }
